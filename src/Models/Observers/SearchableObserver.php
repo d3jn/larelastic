a7a33fd0a85f7b1d6e113fe87dfc59ad9ee1fae3
Serhii Yaniuk <s.yaniuk@ideil.com>
2019-04-19 11:04:08 +0300

    Add softdeletes support to eloquent observer

--- src/Models/Observers/SearchableObserver.php
+++ src/Models/Observers/SearchableObserver.php
@@ -4,6 +4,7 @@ namespace D3jn\Larelastic\Models\Observers;
 
 use D3jn\Larelastic\Contracts\Models\Searchable;
 use Elasticsearch\Client;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SearchableObserver
 {
@@ -33,12 +34,39 @@ class SearchableObserver
     }
 
     /**
+     * Restore passed entity when restore event is triggered.
+     *
+     * @param \D3jn\Larelastic\Contracts\Models\Searchable $entity
+     */
+    public function restore(Searchable $entity): void
+    {
+        $entity->syncToElasticsearch();
+    }
+
+    /**
      * Update passed entity when save event is triggered.
      *
      * @param \D3jn\Larelastic\Contracts\Models\Searchable $entity
      */
     public function saved(Searchable $entity): void
     {
+        // Trashed models shouldn't exist in our index, so we simply ignore save event for them.
+        if ($this->usesSoftDeleting($entity) && $entity->trashed()) {
+            return;
+        }
+
         $entity->syncToElasticsearch();
+    }
+
+    /**
+     * Check if searchable instance utilizes SoftDeletes trait.
+     *
+     * @param \D3jn\Larelastic\Contracts\Models\Searchable $entity
+     *
+     * @return bool
+     */
+    protected function usesSoftDeleting(Searchable $entity): bool
+    {
+        return in_array(SoftDeletes::class, class_uses($entity));
     }
 }
