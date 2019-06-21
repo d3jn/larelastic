041782b93055c523fee76c7a9d2f2817e8827562
Serhii Yaniuk <s.yaniuk@ideil.com>
2019-06-21 18:13:24 +0300

    Add conditional clauses support to Dsl query object

--- src/Query/Dsl.php
+++ src/Query/Dsl.php
@@ -2,8 +2,11 @@
 
 namespace D3jn\Larelastic\Query;
 
+use Closure;
 use D3jn\Larelastic\Contracts\Models\Searchable;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Optional;
 use Illuminate\Support\Traits\Macroable;
 
 class Dsl extends Clause
@@ -38,12 +41,34 @@ class Dsl extends Clause
      * @param string $name
      * @param array  $arguments
      *
-     * @return \D3jn\Larelastic\Query\Clause
+     * @return \D3jn\Larelastic\Query\Clause|\Illuminate\Support\Optional
      */
     public function __call(string $name, array $arguments)
     {
         if (static::hasMacro($name)) {
             return $this->macroCall($name, $arguments);
+        }
+
+        if ($this->isConditional($name, $parsedName)) {
+            $condition = array_shift($arguments);
+
+            if ($this->isConditionMet($condition, $arguments)) {
+                if (isset($arguments[0]) && ($arguments[0] instanceof Closure)) {
+                    $arguments[0] = $arguments[0]->call($this);
+                }
+
+                if (empty($arguments)) {
+                    return static::__get($parsedName);
+                }
+
+                return parent::__call($parsedName, $arguments);
+            } else {
+                if (empty($arguments)) {
+                    return new Optional(null);
+                }
+
+                return $this;
+            }
         }
 
         return parent::__call($name, $arguments);
@@ -140,5 +165,39 @@ class Dsl extends Clause
     public function raw(): array
     {
         return $this->builder->raw();
+    }
+
+    /**
+     * Check if passed name is meant to be conditional.
+     *
+     * @param string $name
+     * @param string &$parsedName
+     *
+     * @return bool
+     */
+    protected function isConditional(string $name, ?string &$parsedName): bool
+    {
+        if (preg_match('/^(?<name>.+)When$/', $name, $matches)) {
+            $parsedName = $matches['name'];
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Check if specified condition is met.
+     *
+     * @param mixed $condition
+     * @param array $arguments
+     *
+     * @return bool
+     */
+    protected function isConditionMet($condition, array $arguments): bool
+    {
+        return ($condition instanceof Closure)
+            ? $condition->call($this, ...$arguments)
+            : (bool) $condition;
     }
 }
